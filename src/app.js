b8a8eee2464a21eb05805e435af8926cd5dd5680
Serhii Verbitskiy <sjcris.me@gmail.com>
2017-07-31 16:06:15 +0300

    get resources stations

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ let a  = 'a';
 console.log('Hello webpack 2', _.isEqual(2,2));
 
 
-function request(obj) {
+function request(obj,done) {
     var xhr = new XMLHttpRequest();
+
     xhr.open(obj.method || "GET", obj.url);
     if (obj.headers) {
         Object.keys(obj.headers).forEach(function(key) {
@@ -16,7 +17,7 @@ function request(obj) {
     }
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 300) {
-          // console.log(xhr.response);
+           done(xhr.response);
         } else {
             console.log(xhr.statusText);
         }
@@ -25,12 +26,62 @@ function request(obj) {
         console.log(xhr.statusText);
     };
     xhr.send(obj.body);
-    return xhr;
+    
+}
+//JSON.parse(data)
+
+// const makeRequest = async () => {
+//  console.log( await request({
+//      url:"https://ecs-xm.icthh.com/cxf/xm-security-rs-api/v1/auth/login",
+//      method:"POST",
+//      headers:{
+//          'Content-Type':'application/json',
+//          'Access-Control-Allow-Origin':'*'
+//     },
+//      body:JSON.stringify({
+//          "credentials": {
+//              "user": "forwidget",
+//              "password": "forwidget" 
+//          }
+//       })
+//    },function(data){
+//    	console.log(">>");
+//    		console.log(data);
+//    } ));
+ 
+// }
+
+
+// makeRequest().then((result) => {
+// 	console.log("then");
+// });
+
+
+function request(obj) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open(obj.method || "GET", obj.url);
+    if (obj.headers) {
+        Object.keys(obj.headers).forEach(function(key) {
+            xhr.setRequestHeader(key, obj.headers[key]);
+        });
+    }
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+           resolve(xhr.response);
+        } else {
+            reject(xhr.statusText);
+        }
+    };
+    xhr.onerror = function() {
+        reject(xhr.statusText);
+    };
+    xhr.send(obj.body);
+  })
 }
 
-
-const makeRequest = async () => {
- console.log(await request({
+request({
      url:"https://ecs-xm.icthh.com/cxf/xm-security-rs-api/v1/auth/login",
      method:"POST",
      headers:{
@@ -43,12 +94,44 @@ const makeRequest = async () => {
              "password": "forwidget" 
          }
       })
-  }))
- return "done"
-}
+})
+.then(function (datums) {
+  var accessToken = JSON.parse(datums).accessToken;
+
+  request({
+     url:"https://ecs-xm.icthh.com/cxf/xm-search-rs-api/v1/search",
+     method:"POST",
+     headers:{
+     	 'XM-CLIENT-ACCESSTOKEN':accessToken,
+         'Content-Type':'application/json',
+         'Access-Control-Allow-Origin':'*'
+    },
+     body:JSON.stringify({  
+		   "entries":[  
+		      {  
+		         "key":{  
+		            "entityType":{  
+		               "name":"RESOURCE_ENTITY" 
+		            }
+		         },
+		         "value":"rd_r_type_code:PHYSICAL_ATOMIC_CP AND entity_state:AVAILABLE" 
+		      }
+		   ],
+		   "pagination":{  
+		      "offset":0,
+		      "limit":1000
+		   }
+  		})
+	}).then(function(data){
+		console.log( JSON.parse(data).entities);
+		// return JSON.parse(data).entities;
+
+	}).catch(function(err){
+		console.error('Augh, there was an error with getting resourcess!', err.statusText);
+	});
 
 
-makeRequest().then((result) => {
-	console.log("then");
-	console.log(result);
+})
+.catch(function (err) {
+  console.error('Augh, there was an error with getting accessToken!', err.statusText);
 });
